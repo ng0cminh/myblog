49580d5a1292b4df679e206967adc8accba5a2f2
ng0cminh <ng0cminh@yahoo.com>
2021-06-28 14:17:06 +0700

    template

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const express = require('express')
 //import router
 const router = require('./routes');
 
+// Import view engine
 const handlebars  = require('express-handlebars');
 
 const app = express()
 
+//  ussage file static
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.engine('hbs', handlebars({
   extname: '.hbs',
@@ -15,6 +18,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
+// use router
 router(app)
 
  
